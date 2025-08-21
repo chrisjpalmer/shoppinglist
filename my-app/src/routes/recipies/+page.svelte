14a9965a2bce1673988a8a7997a88e3d7fb9f2d6
Chris Palmer <chrisjpalmer6@gmail.com>
2025-08-21 17:53:26 +1000

    forming up the grpc server

--- my-app/src/routes/recipies/+page.svelte
+++ my-app/src/routes/recipies/+page.svelte
@@ -1,3 +1,26 @@
+<script lang="ts">
+	import { ShoppingListService } from '../../gen/shopping_list_service_pb';
+	import { createClient } from "@connectrpc/connect";
+	import { createConnectTransport } from "@connectrpc/connect-web";
+  	import type { Meal } from '../../gen/meal_pb';
+
+	const transport = createConnectTransport({
+		baseUrl: "http://localhost:8080",
+	});
+
+	const client = createClient(ShoppingListService, transport);
+
+	let meals: Meal[] = $state([])
+
+	async function refresh() {
+		const rs = await client.getMeals({})
+		meals = rs.meals
+	}
+
+	refresh()
+
+</script>
+
 <svelte:head>
 	<title>Recipies</title>
 	<meta name="description" content="About this app" />
@@ -7,7 +30,8 @@
 	<h1>Recipies</h1>
 
 	<select>
-		<option value="spaghetti-bolognese">Spaghetti Bolognese</option>
-		<option value="not-spaghetti-bolognese">Not Spaghetti Bolognese</option>
+		{#each meals as meal}
+			<option value="{meal.id}">{meal.name}</option>
+		{/each}
 	</select>
 </div>
